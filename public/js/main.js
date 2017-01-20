215587a398fa87942807902c49420b115c4c49e2
Carolyn Lam <caro_lyn_@hotmail.com>
2017-01-20 02:21:52 -0500

    add login, logout, signup functionality with cookies, bcrypt, and uuid

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('twitterApp', ['ui.router']);
+var app = angular.module('twitterApp', ['ui.router', 'ngCookies']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
@@ -8,16 +8,142 @@ app.config(function($stateProvider, $urlRouterProvider) {
     templateUrl: '/templates/world_timeline.html',
     controller: 'WorldTimelineController'
   })
+  .state({
+    name: 'signup',
+    url: '/signup',
+    templateUrl: '/templates/signup.html',
+    controller: 'SignUpController'
+  })
+  .state({
+    name: 'login',
+    url: '/login',
+    templateUrl: '/templates/login.html',
+    controller: 'LoginController'
+  })
+  .state({
+    name: 'profile',
+    url: '/profile/{username}',
+    templateUrl: '/templates/profile.html',
+    controller: 'ProfileController'
+  })
 
   $urlRouterProvider.otherwise('/');
 
 });
 
+app.run(function($rootScope, $cookies, $http) {
+
+  // when page reloads, check if cookies exists
+  // if so, that means a user is currently logged in
+  // because when the page refreshes, we then need to reassign data from
+  // the cookies back to rootScope variables
+
+  var cookieData = $cookies.getObject('cookieData');
+
+  if (cookieData) {
+    // reassign all the rootScope variables
+    $rootScope.rootUsername = cookieData._id;
+  }
+
+  $rootScope.rootLogout = function() {
+    console.log('logging out');
+    var url = '/api/logout';
+    return $http({
+      method: 'PUT',
+      url: url,
+      data: {
+        username: $rootScope.rootUsername
+      }
+    })
+    .then(function() {
+      // reassign rootScope variables to null
+      $rootScope.rootUsername = null;
+      // clear cookies
+      $cookies.remove('cookieData');
+    })
+    .catch(function(err) {
+      console.log('err loggin out...', err.message);
+    });
+  };
+
+});
+
 app.factory('TwitterFactory', function($http) {
   var service = {};
+
+  service.login = function(loginInfo) {
+    console.log('huh?', loginInfo);
+    var url = '/api/login';
+
+    return $http({
+      method: 'POST',
+      url: url,
+      data: loginInfo
+    });
+  };
+
+  service.signUp = function(signUpInfo) {
+    var url = '/api/signup';
+
+    return $http({
+      method: 'PUT',
+      url: url,
+      data: signUpInfo
+    });
+  };
+
   return service;
 });
 
-app.controller('WorldTimelineController', function($state, $scope, TwitterFactory) {
+app.controller('WorldTimelineController', function($rootScope, $state, $scope, TwitterFactory) {
+
+});
+
+app.controller('LoginController', function($rootScope, $state, $scope, $cookies, TwitterFactory) {
+
+  $scope.login = function() {
+    var loginInfo = {
+      username: $scope.username,
+      password: $scope.password
+    };
+    TwitterFactory.login(loginInfo)
+      .then(function(returnedInfo) {
+        console.log('returned:', returnedInfo);
+        if (returnedInfo.data.message) {
+          $cookies.putObject('cookieData', returnedInfo.data.userInfo);
+          $rootScope.rootUsername = returnedInfo.data.userInfo._id;
+          $state.go('profile', { username: $rootScope.rootUsername });
+        } else {
+          //
+        };
+      })
+      .catch(function(err) {
+        console.log('err logging in...', err.message);
+      });
+  };
+
+});
+
+app.controller('SignUpController', function($cookies, $rootScope, $state, $scope, TwitterFactory) {
+
+  $scope.signUp = function() {
+    var signUpInfo = {
+      username: $scope.username,
+      password: $scope.password
+    };
+    TwitterFactory.signUp(signUpInfo)
+      .then(function(returnedInfo) {
+        $cookies.putObject('cookieData', returnedInfo.data.userInfo);
+        $rootScope.rootUsername = returnedInfo.data.userInfo._id;
+        $state.go('profile', { username: $rootScope.rootUsername });
+      })
+      .catch(function(err) {
+        console.log('err signing up...', err.message);
+      });
+  };
+
+});
+
+app.controller('ProfileController', function($cookies, $state, $rootScope, $scope, TwitterFactory) {
 
 });
--- twitter_backend.js
+++ twitter_backend.js
@@ -1,5 +1,6 @@
 const   express       =       require('express'),
         mongoose      =       require("mongoose"),
+        bodyParser    =       require('body-parser'),
         bluebird      =       require("bluebird"),
         uuidV4        =       require("uuid/v4");
 
@@ -13,7 +14,9 @@ const app           = express();
 mongoose.Promise    = bluebird;
 const ObjectId      = mongoose.Schema.ObjectId;
 
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
 
 // NEW DB
 mongoose.connect('mongodb://localhost/redo_twitter_db');
@@ -21,28 +24,149 @@ mongoose.connect('mongodb://localhost/redo_twitter_db');
 // ********************************
 //              SCHEMAS
 // ********************************
+const Tweet = mongoose.model('Tweet', {
+  content: { type: String, required: true },
+  date: Date,
+  author: String,
+  likes: [String], // username
+  retweets: [String] // username
+});
+
 const User = mongoose.model('User', {
   _id: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true, unique: true },
-  lastName: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
+  firstName: String,
+  lastName: String,
+  password: { type: String, required: true },
   followers: [String],
   following: [String],
   likes: [ObjectId],
   tweets: [ObjectId], // tweet _id
-  retweets: [ObjectId] // tweet._id
+  retweets: [ObjectId], // tweet _id
   avatar: String,
   joined: Date,
   authToken: { token: String, expires: Date }
 });
 
-const Tweet = mongoose.model('Tweet' {
-  author: String,
-  date: Date,
-  content: String,
-  likes: [String], // username
-  retweets: [String] // username
+app.put('/api/signup', function(request, response) {
+
+  var data = request.body;
+  var username = data.username;
+  var password = data.password;
+
+  bcrypt.genSalt(saltRounds)
+    .then(function(salt) {
+      return bcrypt.hash(password, salt);
+    })
+    .then(function(hashedPassword) {
+
+      // save the hash form of the password in the db
+
+      var newUser = new User({
+        _id: username,
+        firstName: "",
+        lastName: "",
+        password: hashedPassword,
+        followers: [],
+        following: [],
+        likes: [],
+        tweets: [],
+        retweets: [],
+        avatar: "",
+        joined: new Date(),
+        // update token expires date
+        authToken: { token: uuidV4(), expires: new Date() }
+      });
+
+      return newUser.save();
+    })
+    .then(function(newUser) {
+      console.log('blue');
+      return response.json({
+        userInfo: newUser
+      });
+    })
+    .catch(function(err) {
+      response.status(500);
+      response.json({
+        error: err.message
+      });
+    });
+
 });
+
+app.post('/api/login', function(request, response) {
+
+  var data = request.body;
+  var username = data.username;
+  var password = data.password;
+
+  User.findOne({ _id: username })
+    .then(function(userInfo) {
+      var hashedPassword = userInfo.password;
+
+      return [ userInfo, bcrypt.compare(password, hashedPassword) ];
+    })
+    .spread(function(userInfo, passwordsMatch) {
+      console.log('red');
+      if (passwordsMatch) {
+        return [ User.findOneAndUpdate(
+          { _id: username },
+          // DO: update expires date
+          { $set: { authToken: { token: uuidV4(), expires: new Date() } } }
+        ), passwordsMatch ];
+      }
+      return response.json({
+        message: passwordsMatch
+      });
+    })
+    .spread(function(updatedUser, passwordsMatch) {
+      return response.json({
+        message: passwordsMatch,
+        userInfo: updatedUser
+      });
+    })
+    .catch(function(err) {
+      console.log('err logging in user...', err.message);
+      response.status(500);
+      response.json({
+        error: err.message
+      });
+    });
+
+});
+
+app.put('/api/logout', function(request, response) {
+
+  console.log('username', request.body);
+  var username = request.body.username;
+
+  User.update({
+      _id: username,
+    }, {
+      $set: { authToken: { token: "", expires: "" } }
+    })
+    .then(function(updatedUser) {
+      return response.json({
+        message: "success logging out user"
+      });
+    })
+    .catch(function(err) {
+      console.log('err logging out user...', err.message);
+      response.status(500);
+      response.json({
+        error: err.message
+      });
+    });
+
+});
+
+
+
+
+
+
+
+
 
 app.listen(3005, function() {
   console.log('The server is listening on port 3005....');
